efc1c50284109806ad31743a0ac973e21a328f76
Takanobu INAFUKU <takanobu.inafact@gmail.com>
2021-04-30 12:10:57 +0900

    Fix

--- electron-app/renderer/pages/_app.jsx
+++ electron-app/renderer/pages/_app.jsx
@@ -11,16 +11,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <script src="https://unpkg.com/@tensorflow/tfjs-core@2.7.0/dist/tf-core.js"></script>
-        <script src="https://unpkg.com/@tensorflow/tfjs-converter@2.7.0/dist/tf-converter.js"></script>
-
-        <script src="https://unpkg.com/@tensorflow/tfjs-backend-cpu@2.7.0/dist/tf-backend-cpu.js"></script>
-        <script src="https://unpkg.com/@tensorflow/tfjs-backend-wasm@2.7.0/dist/tf-backend-wasm.js"></script>
-        <script src="https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.7.0/dist/tf-backend-webgl.js"></script>
-
-        <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/facemesh"></script>
-      </Head>
       <Component {...pageProps} />
     </>
   );
--- electron-app/renderer/pages/index.jsx
+++ electron-app/renderer/pages/index.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 
+import Head from 'next/head';
+
 import FaceTracker from '../components/FaceTracker';
 
 export default function IndexPage() {
@@ -23,6 +25,17 @@ export default function IndexPage() {
 
   return (
     <>
+      <Head>
+        <script src="/tf-core.js"></script>
+        <script src="/tf-converter.js"></script>
+
+        <script src="/tf-backend-cpu.js"></script>
+        <script src="/tf-backend-wasm.js"></script>
+        <script src="/tf-backend-webgl.js"></script>
+
+        <script src="/facemesh"></script>
+      </Head>
+
       {message && <p>{message}</p>}
 
       <FaceTracker />
